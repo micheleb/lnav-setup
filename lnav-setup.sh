3850f2195e0a2da4fcfc50c829a7c988eb51f704
Michele Bonazza <michele.bonazza@gmail.com>
2017-04-20 18:47:52 +0200

    Add -l option for deb version

--- lnav-setup.sh
+++ lnav-setup.sh
@@ -7,3 +7,16 @@ fi
 wget -O lnav.deb https://github.com/tstack/lnav/releases/download/v0.8.2/lnav_0.8.2_amd64.deb
 dpkg -i lnav.deb
 rm lnav.deb
+
+if [[ "$#" -eq 1 && "$1" == "-l" ]]; then
+    cd /home/ec2-user
+    if [[ ! -d logs ]]; then
+        mkdir logs
+        cd logs
+        ln -s /var/log/tomcat8/application.log application.log
+        ln -s /var/log/tomcat8/catalina.out catalina.out
+        ln -s /var/log/tomcat8/localhost_access_log.txt
+        cd ..
+        chown -R ec2-user logs
+    fi
+fi
