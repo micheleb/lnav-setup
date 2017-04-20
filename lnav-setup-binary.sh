defa58520b7eddc5156da11b504a89f2602ae38d
Michele Bonazza <michele.bonazza@gmail.com>
2017-04-20 18:11:18 +0200

    Fix zip name

--- lnav-setup-binary.sh
+++ lnav-setup-binary.sh
@@ -7,8 +7,10 @@ fi
 VERSION="0.8.2"
 wget -O lnav-${VERSION}.zip https://github.com/tstack/lnav/releases/download/v${VERSION}/lnav-${VERSION}-linux-64bit.zip
 unzip lnav-${VERSION}.zip
-rm lnav.zip
+rm lnav-${VERSION}.zip
+rm -rf /opt/lnav
 mv lnav-${VERSION} /opt/lnav
 cd /usr/local/bin
+rm -r lnav
 ln -s /opt/lnav/lnav lnav
 
